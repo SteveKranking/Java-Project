82d5c62df82dcba6284db47fb6a04f72df3e4280
Stephen Kranking <stephenkranking@yahoo.com>
2018-07-27 12:22:18 -0400

    a few notes for future plans

---
+++ src/main/java/com/project/Bracket_Lottery/services/MatchService.java
@@ -0,0 +1,18 @@
+package com.project.Bracket_Lottery.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchService {
+	// Member variables / service initializations go here
+		
+	public MatchService(){
+
+	}
+	
+	// Crud methods to act on services go here.
+}
